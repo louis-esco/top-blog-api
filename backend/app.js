5bc862f645d8b97b901a902fa394f395089050f9
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-11-02 16:22:37 +0100

    Setup prisma and routes

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import passport from "passport";
+import "dotenv/config";
+import routes from "./routes";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use("/posts", routes.posts);
+app.use("/users", routes.users);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
