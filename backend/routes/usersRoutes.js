ad8c45a325cde367d4e9f9e798082d28ece2d981
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-11-07 21:08:00 +0100

    Unprotect auth route

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -4,15 +4,15 @@ import * as authController from "../controllers/authController.js";
 
 const router = Router();
 
+router.post("/auth", authController.userLogin, (req, res) => {
+  res.json(req.user);
+});
+
 router.use(authController.verifyJwt);
 
 router.get("/", usersController.test, usersController.getUsers);
 router.post("/", usersController.postCreateUser);
 router.put("/:userId", usersController.putUpdateUser);
 router.delete("/:userId", usersController.deleteUser);
-
-router.post("/auth", authController.userLogin, (req, res) => {
-  res.json(req.user);
-});
 
 export default router;
